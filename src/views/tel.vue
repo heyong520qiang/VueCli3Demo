7cae42a2378670bbd97fbe5ef76eed70e416efeb
bit <heyong520qiang@163.com>
2019-06-13 16:59:25 +0800

    00

---
+++ src/views/tel.vue
@@ -0,0 +1,22 @@
+<template>
+ <div class="tel">
+tel:
+ </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
