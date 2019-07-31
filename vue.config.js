ee6fcb7e5864dd3c6ec4955ffb5dbae0af6b686f
bit <heyong520qiang@163.com>
2019-07-31 11:39:29 +0800

    00

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 const resolve = dir => path.join(__dirname, dir)
 
-const BASE_URL = process.env.NODE_ENV === 'production' ? '/dist/' : '/'
+const BASE_URL = process.env.NODE_ENV === 'production' ? './' : '/'
 module.exports = {
   lintOnSave: false,
   publicPath: BASE_URL,
@@ -10,6 +10,12 @@ module.exports = {
     config.resolve.alias
       .set('@', resolve('src'))
       .set('_c', resolve('src/components'))
+  },
+  css: {
+    modules: false, // 启用 CSS modules
+    extract: true, // 是否使用css分离插件
+    sourceMap: false, // 开启 CSS source maps?
+    loaderOptions: {} // css预设器配置项
   },
   productionSourceMap: false,
   devServer: {// 跨域代理设置
